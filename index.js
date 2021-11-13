dbe9756e1394632a9dfce35cf3353f161c104ba2
HexMagnus <luke.dev.fullstack@outlook.com>
2021-11-13 17:25:15 -0300

    Update 1-4

---
+++ index.js
@@ -0,0 +1,30 @@
+//let altura= prompt ("Qual é a a altura")
+//let largura=prompt ("Qual é a largura")
+//let area = altura * largura  
+
+
+//console.log ("A área do retangulo é", area)
+ 
+//------------------------------------------------------------------------------------------------------------------------//
+// let anoatual = prompt("Em que anos estamos")
+//let anonasci = prompt ("Em que ano você nasceu")
+// let resultado = anoatual - anonasci
+
+ //console.log("Sua idade é ", resultado)
+
+ //------------------------------------------------------------------------------------------------------------------------//
+
+ //let pesoemkg = prompt("Qual é o seu peso?")
+ //let alturametros = prompt ("Qual a sua altura")
+ //let alturaxaltura = alturametros * 2
+ //let imc = pesoemkg / alturaxaltura  
+
+ //console.log ("Seu IMC é ", imc)
+
+ //------------------------------------------------------------------------------------------------------------------------//
+
+ //let nome = prompt("Meu nome é")
+ //let idade = prompt ("tenho")
+ //let email= prompt ("e o meu email é")
+
+ //console.log ("Meu nome é", nome,",tenho", idade, "anos, e o meu email é", email)
